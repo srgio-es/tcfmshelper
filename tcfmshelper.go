b83ba190faf31b7cd4689c65a1c537819e99bc0a
Sergio Soto <me@srgio.es>
2021-03-25 12:19:50 +0100

    feat: Improve FSC Dashboard visuals

--- tcfmshelper.go
+++ tcfmshelper.go
@@ -1,35 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
+	"os"
 
-	"github.com/gin-gonic/gin"
-
-	"github.com/srgio-es/tcfmshelper/routers"
+	"github.com/srgio-es/tcfmshelper/server"
 	"github.com/srgio-es/tcfmshelper/settings"
 )
 
 func init() {
-	log.Println("Starting tcfmshelper")
 	settings.Setup()
 }
 
 func main() {
-	gin.SetMode(settings.ServerSettings.RunMode)
-
-	routers := routers.InitRouter()
-	endPoint := fmt.Sprintf(":%d", settings.ServerSettings.Port)
-
-	server := &http.Server{
-		Addr:         endPoint,
-		Handler:      routers,
-		ReadTimeout:  settings.ServerSettings.ReadTimeout,
-		WriteTimeout: settings.ServerSettings.WriteTimeout,
+	err := server.Run()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
 	}
-
-	log.Printf("Start http server listening %s", endPoint)
-
-	server.ListenAndServe()
 }
